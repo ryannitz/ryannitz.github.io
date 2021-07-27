c8d0ad280e2a4fb931f83358d2387266440693ef
Ryan Nitz <rnitz@unb.ca>
2021-07-26 23:04:53 -0300

    Initial commit for psyc3033 proj

---
+++ health-behavior/main.js
@@ -0,0 +1,220 @@
+var COL_DATE = 0;
+var COL_BEDTIME = 1;
+var COL_ASLEEPTIME = 2;
+var COL_WAKEUPTIME = 3;
+var COL_AWAKETIME = 4;
+var COL_NAPTIME = 5;
+var COL_TOTALSLEEPTIME = 6;
+
+var before = [
+    //date       , bedtime  , fallAslpee,wakeup,    time awake, nap time, total slept
+    ["2021-06-28","00:00:00","02:26:00","08:18:00","00:11:00","00:00:00","05:41:00"],
+    ["2021-06-29","00:00:00","02:54:00","09:48:00","00:40:00","01:25:00","07:39:00"],
+    ["2021-06-30","00:00:00","01:51:00","10:00:00","00:46:00","01:06:00","08:29:00"],
+    ["2021-07-01","00:00:00","01:42:00","11:30:00","00:36:00","00:00:00","09:12:00"],
+    ["2021-07-02","00:00:00","03:04:00","09:58:00","00:54:00","01:29:00","07:29:00"],
+    ["2021-07-03","00:00:00","02:25:00","06:15:00","00:14:00","02:36:00","06:12:00"],
+    ["2021-07-04","00:00:00","02:27:00","11:54:00","00:41:00","00:00:00","08:46:00"],
+    ["2021-07-05","00:00:00","11:45:00","06:35:00","00:48:00","01:25:00","07:27:00"],
+    ["2021-07-06","00:00:00","02:08:00","09:01:00","00:38:00","01:07:00","07:22:00"],
+    ["2021-07-07","00:00:00","01:41:00","08:57:00","00:35:00","00:00:00","06:41:00"],
+    ["2021-07-08","00:00:00","01:37:00","09:08:00","00:36:00","01:00:00","07:55:00"],
+    ["2021-07-09","00:00:00","03:07:00","09:09:00","00:31:00","00:00:00","05:31:00"],
+    ["2021-07-10","00:00:00","04:55:00","13:02:00","00:35:00","00:00:00","07:32:00"],
+    ["2021-07-11","00:00:00","02:06:00","06:11:00","00:30:00","02:18:00","05:53:00"],
+]
+var during = [
+    ["2021-07-12","02:30:00","03:02:00","09:13:00","00:38:00","02:22:00","07:55:00"],
+    ["2021-07-13","02:00:00","02:25:00","09:13:00","00:40:00","00:00:00","06:08:00"],
+    ["2021-07-14","01:00:00","01:30:00","09:13:00","00:40:00","00:00:00","07:03:00"],
+    ["2021-07-15","12:45:00","02:10:00","08:00:00","00:33:00","00:00:00","05:17:00"],
+    ["2021-07-16","12:30:00","02:21:00","08:00:00","00:32:00","00:00:00","05:07:00"],
+    ["2021-07-17","02:30:00","02:49:00","12:26:00","00:43:00","00:00:00","08:54:00"],
+    ["2021-07-18","01:30:00","02:04:00","11:00:00","00:58:00","00:00:00","07:58:00"],
+    ["2021-07-19","23:00:00","23:34:00","06:29:00","00:54:00","01:31:00","07:32:00"],
+    ["2021-07-20","01:30:00","02:10:00","09:04:00","00:43:00","00:00:00","06:11:00"],
+    ["2021-07-21","02:00:00","02:51:00","09:50:00","00:58:00","00:00:00","06:01:00"],
+    ["2021-07-22","02:00:00","02:34:00","09:01:00","00:26:00","00:00:00","06:01:00"],
+    ["2021-07-23","03:00:00","03:26:00","09:00:00","00:34:00","00:00:00","05:58:00"],
+    ["2021-07-24","00:00:00","04:19:00","11:35:00","00:25:00","00:00:00","06:51:00"],
+    ["2021-07-25","00:00:00","03:32:00","11:17:00","00:43:00","00:00:00","07:01:00"]
+]
+
+
+
+
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+
+
+
+  $(window).scroll(function(){
+    if(window.scrollY > ($("#header-mast").offset().top + $("#header-mast").height())){
+      $("#layout-nav").slideDown()
+    }else{
+      $("#layout-nav").slideUp()
+    }
+  });
+
+
+
+  Highcharts.chart('highchart', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Hours slept before behavior change'
+    },
+    xAxis: {
+        categories: getDateLabels(before),
+        plotBands: [// visualize the weekend
+            {
+                from: 4.5,
+                to: 6.5,
+                color: 'rgba(255, 0, 0, .2)'
+            },
+            {
+                from: 11.5,
+                to: 13.5,
+                color: 'rgba(255, 0, 0, .2)'
+            },
+        ]
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Total hours slept'
+        },
+        stackLabels: {
+            enabled: true,
+            style: {
+                fontWeight: 'bold',
+                color: ( // theme
+                    Highcharts.defaultOptions.title.style &&
+                    Highcharts.defaultOptions.title.style.color
+                ) || 'gray'
+            }
+        }
+    },
+    legend: {
+        align: 'right',
+        x: -30,
+        verticalAlign: 'top',
+        y: 25,
+        floating: true,
+        backgroundColor:
+            Highcharts.defaultOptions.legend.backgroundColor || 'white',
+        borderColor: '#CCC',
+        borderWidth: 1,
+        shadow: false
+    },
+    tooltip: {
+        headerFormat: '<b>{point.x}</b><br/>',
+        pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+    },
+    plotOptions: {
+        column: {
+            stacking: 'normal',
+            dataLabels: {
+                enabled: true
+            }
+        }
+    },
+    series: [
+        {
+            name: 'Nap',
+            data: getNapTimesInHours(before)
+        },
+        {
+            name: 'Sleep',
+            data: getTotalSleepTimeWithoutNap(before)
+        }
+    ]
+});
+
+
+
+function getColumnData(data, colIndex){
+    var dataset = [];
+    data.forEach(function(item, index){
+        dataset.push(item[colIndex]);
+    });
+    return dataset;
+}
+
+function getDateCol(data){
+    return getColumnData(data, COL_DATE)
+}
+
+function getBedTimeCol(data){
+    return getColumnData(data, COL_BEDTIME)
+}
+
+function getAsleepTimeCol(data){
+    return getColumnData(data, COL_ASLEEPTIME)
+}
+
+function getWakeupTimeCol(data){
+    return getColumnData(data, COL_WAKEUPTIME)
+}
+
+function getAwakeTimeCol(data){
+    return getColumnData(data, COL_AWAKETIME)
+}
+
+function getNapTimeCol(data){
+    return getColumnData(data, COL_NAPTIME)
+}
+
+function getTotalSleepTimeCol(data){
+    return getColumnData(data, COL_TOTALSLEEPTIME)
+}
+
+function convertToUTC(data){
+
+}
+
+function formatDate(value) {
+    let date = new Date(value);
+    const day = date.toLocaleString('default', { day: '2-digit' });
+    const month = date.toLocaleString('default', { month: 'short' });
+    const year = date.toLocaleString('default', { year: 'numeric' });
+    return day + '-' + month;
+}
+
+
+function getDateLabels(data){
+    var dates = getDateCol(data);
+    var dateLabels = []
+    dates.forEach(function(item, index){
+        dateLabels.push(formatDate(item));
+    });
+    return dateLabels;
+}
+
+function getNapTimesInHours(data){
+    var napTimes = getNapTimeCol(data);
+    var napTimesInHours = []
+
+    napTimes.forEach(function(item, index){
+        var date = new Date(("2021-07-10 " + item));
+        var num  = (date.getHours() + date.getMinutes()/60).toFixed(2);
+        napTimesInHours.push(parseFloat(num));
+    });
+    return napTimesInHours;
+}
+
+function getTotalSleepTimeWithoutNap(data){
+    var napTimesInHours = getNapTimesInHours(data);
+    var totalSleep =  getTotalSleepTimeCol(data);
+    var totalSleepWithoutNaps = [];
+
+    totalSleep.forEach(function(item, index){
+        var date = new Date(("2021-07-10 " + item));
+        var num  = (date.getHours() + date.getMinutes()/60 - napTimesInHours[index]).toFixed(2);
+        totalSleepWithoutNaps.push(parseFloat(num));
+    });
+    return totalSleepWithoutNaps;
+}
+
+});
---
+++ health-behavior/vueControllers.js
@@ -0,0 +1,156 @@
+
+//todo: Graph showing the bedtime pre-behavior change 
+//todo: Graph showing the total amount of sleep for the week pre-behavior change (without naps)
+
+//todo: Graph showing the bedtime and real bedtime post-behavior change
+//todo: Graph showing the total amount of sleep for the wee post-behavior change
+//todo: Grapgh showing how long it took to fall asleep
+
+//todo: Grapgh 
+
+
+var app = new Vue({
+    el: "#app",
+
+  //------- data --------
+    data: {
+
+        sections : [
+            {
+                title: "The Pandemic",
+                content: "The covid-19 pandemic has changed lives around the globe. People have been forced to change their day-to-day behaviors and ingrained habits. Some behaviors have improved given the extra time during quaratine, other -less desireable- behaviors were able to grab hold with lessened routine and structure. My not-so-desirable behavior that got worse was sleep. This website will be a quick overview with graphs and logs documenting my what my sleep has deteriorated to, and where I'm trying to get it back to."
+            },
+            {
+                title: "Pre-Covid Sleep",
+                content: "Being in University there were obviously times I would stay up until 3am for assignments and studying. I would always stay up past 12 anyway no matter the circumstance. Occasionally I would pull all nighters or stay up extremely late while working on passionate computer science projects (sort of like this one). One thing that was always consistent though was the time I had to wake up (7:30am). My wake up time would keep me on schedule and make me tired enough to fix my sleep schedule the very same day. This obviously isn't the case with pandemic and remote work."
+            },
+            {
+                title: "Covid Sleep Downfall",
+                content: "Once the pandemic hit, suddenly there was no reason to wake up a few hours early to get ready for the day and make the required comute to school and work. The fact that I no longer needed to get up so early meant I could stay up later the night beforehand. Of course this bed time got later and later as my sleep schedule would adjust and the extra time to sleep turned into the minimal amount of sleep just like the pre-pandemic. It didn't stop there though... With school and work being remote, the gaps in my schedule and immediate amenties of my house meant that I could nap in my free time to make up for the sleep loss. With the extra napping, the problems continued to compound up to the point I began sleeping at inapropriate times. Eventually my entire schedule collapsed. Showering, eating, sleeping, all completely random now."
+            },
+            {
+                title: "Behavior Change Goals and Methods",
+                content: ""
+            }
+        ],
+
+        week_enum: [
+            "MON",
+            "TUE",
+            "WED",
+            "THU",
+            "FRI",
+            "SAT",
+            "SUN"
+        ],
+
+        w1: [
+            {
+                log: "Last night was the first day of my behavior change experiment. Due to this being the first night, I wasn't worried about when I went to bed, but when I wake up to kick of my tiredness schedule. Unfortunately I crumbled and had a midday nap.",
+                date : "2021-07-12",
+                timedata: []
+            },
+            {
+                log: "Last night I fell asleep late again but did not nap during the day. I was up late catching up on lectures from the class I had to miss Monday. Coffee kept me awake for the first hours of the day, hopefully I can fall asleep at a good time tonight.",
+                date: "2021-07-13",
+                timedata: []
+            },
+            {
+                log: "Last night I couldn't fall asleep when I wanted because of the heat. I forced myself to wake up at my goal time though.",
+                date: "2021-07-14",
+                timedata: []
+            },
+            {
+                log: "Last night was probably the closest I've gotten to my goal. Very happy, not much to say. Felt great all day.",
+                date: "2021-07-15",
+                timedata: []
+            },
+            {
+                log: "Last night I tried to get to bed early but the heat kept my tossing and turning. :( This is becoming a challenge with no A/C in my room.",
+                date: "2021-07-16",
+                timedata: []
+            },
+            {
+                log: "Weekend write off. ",
+                date: "2021-07-17",
+                timedata: []
+            },
+            {
+                log: "Weekend write off",
+                date: "2021-07-18",
+                timedata: []
+            }
+           
+       ],
+        w2: [
+            {
+                log: "Lorem",
+                date: "2021-07-19",
+                timedata: []
+            },
+            {
+                log: "Lorem",
+                date: "2021-07-20",
+                timedata: []
+            },
+            {
+                log: "Lorem",
+                date: "2021-07-21",
+                timedata: []
+            },
+            {
+                log: "Lorem",
+                date: "2021-07-22",
+                timedata: []
+            },
+            {
+                log: "Lorem",
+                date: "2021-07-23",
+                timedata: []
+            },
+            {
+                log: "Lorem",
+                date: "2021-07-24",
+                timedata: []
+            },
+            {
+                log: "Lorem",
+                date: "2021-07-25",
+                timedata: []
+            }
+        ]
+    },
+
+  //------- methods --------
+    methods: {
+        
+    },
+
+    computed: {
+        // filteredUsers() {
+        //     return this.users.filter(user => {
+        //         return user.userName.toLowerCase().indexOf(this.searchUser.toLowerCase()) > -1
+        //     })
+        // }
+    },
+
+    mounted: function(){
+        
+    }
+
+});
+
+function createAlert(type, text, millis){
+    var id = Math.floor((Math.random() * 1000) + 1);
+    var alerthtml = '<div id="alert-'+id+'" class="alert alert-'+type+' alert-dismissible text-center fixed-bottom w-75 mx-auto mb-5">' +
+                    '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+                    '<strong><i class="fas fa-info-circle"></i></strong> '+
+                    text+
+                '</div>'
+    $("body").append(alerthtml);
+    setTimeout(function(){
+        $('#alert-'+id).fadeOut(500, function(){
+            $('#alert-'+id).remove();
+        });
+    }, millis)
+  }
