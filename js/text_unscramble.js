64f150f4658e6f43aedc4d78f0350ad560c83fd2
Ryan Nitz <ryan.nitz01@gmail.com>
2022-12-07 16:50:58 -0400

    Isolated text unscramble code

--- js/text_unscramble.js
+++ js/text_unscramble.js
@@ -1,36 +1,3 @@
-
-
-// Example showing how to initialize with vanilla js
-
-var largeExample = document.getElementById("largeExample");
-largeExample.addEventListener("click", function() {
-    unscramble(1000, 25, this, this);
-});
-
-//Without jQuery, you would need to loop through each element provided by getElementsByClass
-$(document).ready(function(){
-    $(".list-example").mouseenter(function() {
-        unscramble(1000, 25, this, this);
-    });
-
-    $("#textToScramble").on("input", function() {
-        //need to move input value to a text-holding element.
-        $("#scrambleTestResult").text($(this).val());
-    });
-    //Example showing how to use the trigger from a different event/element.
-    $("#scrambleTestSubmit").click(function() {
-        var element = document.getElementById("scrambleTestResult");
-        unscramble($("#unscrambleSpeed").val(), $("#randomizerSpeed").val(), element, element);
-    })
-
-    $("#targetSourceSubmit").click(function() {
-        var sourceElement = document.getElementById("sourceExample");
-        var targetElement = document.getElementById("targetExample");
-        unscramble(2000, 25, sourceElement, targetElement);
-    })
-});
-
-
 function unscramble(unscrambleSpeed, randomizerSpeed, sourceElement, targetElement) {
     if(targetElement.classList.contains("unscrambling")) {
         return;
