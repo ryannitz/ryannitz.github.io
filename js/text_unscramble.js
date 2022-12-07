cc942b0c16ed261ab1192fb8a959ad9de263ff28
Ryan Nitz <ryan.nitz01@gmail.com>
2022-12-07 16:47:11 -0400

    Text unscramble site complete

--- js/text_unscramble.js
+++ js/text_unscramble.js
@@ -4,40 +4,44 @@
 
 var largeExample = document.getElementById("largeExample");
 largeExample.addEventListener("click", function() {
-    unscramble(1000, 25, this);
+    unscramble(1000, 25, this, this);
 });
 
 //Without jQuery, you would need to loop through each element provided by getElementsByClass
 $(document).ready(function(){
     $(".list-example").mouseenter(function() {
-        unscramble(1000, 25, this);
+        unscramble(1000, 25, this, this);
     });
 
     $("#textToScramble").on("input", function() {
-        //need to move input to text-holding element.
-        //may consider implementing a source/target for the algorithm
+        //need to move input value to a text-holding element.
         $("#scrambleTestResult").text($(this).val());
     });
-
     //Example showing how to use the trigger from a different event/element.
     $("#scrambleTestSubmit").click(function() {
         var element = document.getElementById("scrambleTestResult");
-        unscramble($("#unscrambleSpeed").val(), $("#randomizerSpeed").val(), element);
+        unscramble($("#unscrambleSpeed").val(), $("#randomizerSpeed").val(), element, element);
+    })
+
+    $("#targetSourceSubmit").click(function() {
+        var sourceElement = document.getElementById("sourceExample");
+        var targetElement = document.getElementById("targetExample");
+        unscramble(2000, 25, sourceElement, targetElement);
     })
 });
 
 
-function unscramble(unscrambleSpeed, randomizerSpeed, element) {
-    if(element.classList.contains("unscrambling")) {
+function unscramble(unscrambleSpeed, randomizerSpeed, sourceElement, targetElement) {
+    if(targetElement.classList.contains("unscrambling")) {
         return;
     }else{
-        element.classList.add("unscrambling");
+        targetElement.classList.add("unscrambling");
     
         var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ';
         var charactersLength = characters.length;
 
-        var textsrc = element
-        var text = element.innerText;
+        var textsrc = sourceElement
+        var text = sourceElement.innerText;
 
         var resolvedText = "";
         var unresolvedCharCount = text.length;
@@ -60,15 +64,14 @@ function unscramble(unscrambleSpeed, randomizerSpeed, element) {
                             */
                             unresolved += characters.charAt(Math.floor(Math.random() * charactersLength));
                         }
-                        textsrc.innerText = resolvedText + unresolved;
-                        console.log("AHHH");
+                        targetElement.innerText = resolvedText + unresolved;
                     }, randomizerSpeed)
                     print();
                     setTimeout(function() {
                         clearInterval(innerInterval);
                     }, charResolveSpeed);
                 }else {
-                    element.classList.remove("unscrambling");
+                    targetElement.classList.remove("unscrambling");
                 }
                 unresolvedCharCount--;
             }, charResolveSpeed);
