3995aa60c065a55bef2a5c0a43983b256af774e7
Ryan Nitz <ryan.nitz01@gmail.com>
2024-05-27 17:33:20 -0600

    Ugh

--- gencreator/gencreator.js
+++ gencreator/gencreator.js
@@ -35,13 +35,6 @@ var app = new Vue({
 
     methods: {
 
-        uploadGenData() {
-            const fileElem = $("#uploadedFile")
-            if (fileElem) {
-              fileElem.click();
-            }
-        },
-
         cleanInput(text){
 
             //find the two encapsulating curly braces
@@ -56,6 +49,7 @@ var app = new Vue({
 
         parseQuestions(text){
             if(text){
+                console.log(text)
                 //cannot parse json with ' symbols
                 text = text.replaceAll("\\'", "APOST")
                 text = text.replaceAll("\'", "\"")
@@ -82,7 +76,7 @@ var app = new Vue({
 
 
         loadGenText(){
-            var text = $("#textfield").val()
+            var text = $("#plainTextGen").val()
             if(!text){
                 console.log("No value in text field")
                 createAlert(alertType.danger, alertLocation.top, 5000, "No valid value provided in text field. Reference the instructions prompt for more...")
@@ -97,9 +91,8 @@ var app = new Vue({
             }
             console.log(text)
             try {
-                this.questions = JSON.parse(text)
-                this.multiAnswerQuestions = []
-                this.initTest();
+                this.creatorQuestions = JSON.parse(text)
+                console.log(this.creatorQuestions)
             } catch (error) {
                 createAlert(alertType.danger, alertLocation.top, -1, "Could not parse the input. Standar format rule was broken")
                 console.log(error)
@@ -112,13 +105,14 @@ var app = new Vue({
                 var key = Object.keys(this.creatorQuestions)[i]
                 var val = this.creatorQuestions[key];
 
-                key = key.replaceAll(`\'`, `\\\'`)
                 key = key.substring(1,key.length-1)
-                key = key.replaceAll(`\"`, `\\\"`)
-
+                console.log(key)
+                key = key.replaceAll(`\'`, `\\\'`)
                 val = val.replaceAll(`\"`, `\'`)
+                key = key.replaceAll(`\n`, `\\n`)
             
                 var questionText = `'${key}':'${val}', \n`;
+                console.log(questionText)
                 genTextField += questionText
                 
             }
@@ -163,13 +157,16 @@ var app = new Vue({
         },
 
         populateUIfromQuestion(question){
-
             this.newQuestion = question.substring(1, question.indexOf(` \\n a. `))
             this.newAnswerA = question.substring(question.indexOf(` \\n a. `)+7, question.indexOf(` \\n b. `))
             this.newAnswerB = question.substring(question.indexOf(` \\n b. `)+7, question.indexOf(` \\n c. `))
             this.newAnswerC = question.substring(question.indexOf(` \\n c. `)+7, question.indexOf(` \\n d. `))
             this.newAnswerD = question.substring(question.indexOf(` \\n d. `)+7, question.length-1)
-
+            console.log(this.newQuestion)
+            console.log(this.newAnswerA)
+            console.log(this.newAnswerB)
+            console.log(this.newAnswerC)
+            console.log(this.newAnswerD)
         }
 
     },
@@ -201,7 +198,7 @@ var app = new Vue({
 });
 
 $(document).ready(function(){
-    window.onbeforeunload = function(){return "Leaving?"}
+    //window.onbeforeunload = function(){return "Leaving?"}
     $("form").submit(function (e) { 
         e.preventDefault();
     });
