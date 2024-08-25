91da1fb181fcd87fb112433c0e0d011c3b7dc12e
Alexandre Daigneault <97113885+Alexandre867@users.noreply.github.com>
2024-08-25 11:56:33 -0400

    Gentester to properly detect 2 checklists

--- gentester/gentester.js
+++ gentester/gentester.js
@@ -242,7 +242,7 @@ var app = new Vue({
                     text = text.replace("{}", "")
                 }
                 text = text.substring(text.indexOf("{")+1, text.indexOf("}"))
-                if(text.includes("checklist") && text.includes("checklist2")){
+                if(text.includes("checklist") && text.includes("checklist2 =")){
 	                text = text + ", " + text.substring(text.indexOf("{", text.indexOf("checklist2"))+1, text.indexOf("}", text.indexOf("checklist2")))
                 }
             }
